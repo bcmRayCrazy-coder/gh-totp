b1722d050f7db04f91ed7f5069ecd161aff18af0
PoolloverNathan <nathan.kulzer@protonmail.com>
2023-06-16 02:26:13 -0400

    fix EVERYTHIGN

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 /// <reference path="types.d.ts" />
 
-const token = "BFIIR4BUU7HFTKTB"
+const token = "4I5YE5JJUHPUSYBI"
 
 const oldNow = Date.now.bind(Date)
-Date.now = () => Math.floor(oldNow() / 6)
+Date.now = () => Math.floor(oldNow() / 30000) * 30000
+const nextNow = () => Math.floor(oldNow() / 30000) * 30000 + 30000
 
 let oldCode = ""
 
@@ -13,4 +14,8 @@ setInterval(async () => {
         oldCode =
         code.innerText = newCode
     }
+    let val = Date.now()
+    pg.max = nextNow() - val
+    pg.value = oldNow() - val
+    pg.value = pg.max - pg.value
 }, 100)
